f5cfa1c917d0cb20180e1848dea83db39005974e
Melissa <lissak230@gmail.com>
2024-11-06 21:00:47 -0300

    agrego test ordentitulo

--- src/clases/ordenarTitulo.ts
+++ src/clases/ordenarTitulo.ts
@@ -1,13 +1,10 @@
 import { AlgoritmoOrdenamiento } from "../interfaces/ordenar";
 import { Tarea } from "./tarea";
 
-export class OrdenTitulo implements AlgoritmoOrdenamiento{
+export class OrdenTitulo implements AlgoritmoOrdenamiento {
 
-    public ordenar(t: Array<Tarea>):void {
-        t.sort((a, b) => {
-            const valorA = a.getTitulo();
-            const valorB = b.getTitulo();
-            return valorA.localeCompare(valorB);   
-        });
+    public ordenar(t: Array<Tarea>): void {
+        t.sort((a, b) => a.getTitulo().localeCompare(b.getTitulo(), undefined, { sensitivity: "base" }));
+        return;
     }
 }
---
+++ tests/ordenarTitulo.test.ts
@@ -0,0 +1,27 @@
+import { mock } from 'jest-mock-extended';
+import { Tarea } from "../src/clases/tarea";
+import { OrdenTitulo } from "../src/clases/ordenarTitulo";
+
+describe('OrdenarTitulo', () => {
+    let ordenador = new OrdenTitulo();
+
+    let lista = new Array();
+
+    let tarea1 = mock<Tarea>();
+    let tarea2 = mock<Tarea>();
+    let tarea3 = mock<Tarea>();
+
+    tarea1.getTitulo.mockReturnValue("Terminar informe");
+    tarea2.getTitulo.mockReturnValue("Llamar a Beto");
+    tarea3.getTitulo.mockReturnValue("decorar la casa");
+
+    beforeEach(() => {
+        lista = [tarea1, tarea2, tarea3];
+    });
+
+    it('Debe poder devolver el array ordenado alfabeticamente', () => {
+        ordenador.ordenar(lista);
+        expect(lista).toEqual([tarea3, tarea2, tarea1]);
+    });
+
+});
